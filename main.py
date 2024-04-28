23bad1c747f7bdd54c5e36d763c96564bd357d04
SemGru <s.m.grushko@gmail.com>
2024-04-28 11:06:11 +0300

    start

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
+ф = 0
 
 class User_start():
     def __init__(self):
