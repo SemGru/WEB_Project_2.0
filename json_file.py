f487cdad472291b332255cdae591c5320bdac16a
SemGru <s.m.grushko@gmail.com>
2024-04-28 14:04:02 +0300

    start

--- json_file.py
+++ json_file.py
@@ -1,7 +1,27 @@
-# import datetime
-#
-# print(datetime.date.today())
-#
-#
-# def res_json(name, clas, desision):
-#     res_dict = {datetime.date.today(): 1}
+import datetime
+import json
+
+
+def res_file(name, clas, res):
+    key = f"{name} {clas}"
+    time_dict = {key: res}
+    time = str(datetime.date.today())[5:]
+    with open('result.json', encoding='utf-8') as f:
+        data = json.load(f)
+        if time in data:
+            data[time].update(time_dict)
+            with open('result.json', 'w', encoding='utf-8') as outfile:
+                json.dump(data, outfile, ensure_ascii=False, indent=2)
+        else:
+            with open('result.json', 'w', encoding='utf-8') as outfile:
+                json.dump({str(time)[5:]: time_dict}, outfile, ensure_ascii=False, indent=2)
+    # time = datetime.date.today()
+    # json_dict = {str(time)[5:]: time_dict}
+
+    # with open('result.json') as f:
+    #     templates = json.load(f)
+    # with open('result.json', 'w') as file:
+    #     json.dump(json_dict, file)
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import json
+
 from flask import Flask, render_template, redirect, request, abort
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-
+import os
+from werkzeug.utils import secure_filename
+import datetime
 from forms.user import RegisterForm, LoginForm
 from data.users import User
 from data import db_session
@@ -8,12 +12,23 @@ import random
 import sqlite3
 from flask import Flask, url_for, request, render_template, redirect, request, abort
 from flask import send_file
+from json_file import res_file
+
 # from admin_files import new_task_to_db
 # from new_task_to_db import new_task_to_db
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+UPLOAD_FOLDER = 'static/img/'
+
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
 
 class User_start():
     def __init__(self):
@@ -141,16 +156,31 @@ def admin_page():
     elif request.method == 'POST':
         # print(request.form.get("file_photo", ""))
         # print(request.form.get("num", ""))
-        new_task(request.form.get("file_photo", ""), request.form.get("num", ""))
+        new_task(request.form.get("file_photo", ""), request.form.get("num", ""), request.files['file_photo'])
         return render_template('admin_page.html')
 
 
+@app.route('/download_json', methods=['POST', 'GET'])
+def download_json():
+    return send_file('result.json')
+
+
 def res_json():
-    print(my_user.clas)
-    print(my_user.name)
+    # print(my_user.clas)
+    # print(my_user.name)
     for i in range(3):
         my_user.result.append([my_user.res_task[i], my_user.res_des[i]])
-    print(my_user.result)
+    # print(my_user.result)
+    res_file(my_user.name, my_user.clas, my_user.result)
+    # key = f"{my_user.name} {my_user.clas}"
+    # time_dict = {key: my_user.result}
+    # json_dict = {datetime.date.today(): time_dict}
+    # print(json_dict)
+    # key = f"{my_user.name} {my_user.clas}"
+    # time_dict = {key: my_user.result}
+    # json_dict = {datetime.date.today(): time_dict}
+    # with open('result.json') as file:
+    #     json.dump(json_dict, file)
 
 
 def tasks(my_counter, img):
@@ -158,9 +188,20 @@ def tasks(my_counter, img):
     return render_template('tasks_page.html', img=img)
 
 
-def new_task(photo_name, num):
-    if int(num) in [1, 2, 3]:
-        print(photo_name, num)
+def new_task(photo_name, num, file):
+    # if int(num) in [1, 2, 3]:
+    #     print(photo_name, num)
+    # if int(num) in [1, 2, 3] and allowed_file(photo_name):
+    #     filename = secure_filename(photo_name)
+    #     request.files['file'].save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #     # print('upload_image filename: ' + filename)
+    #     return render_template('admin_page.html')
+    # else:
+    return render_template('admin_page.html')
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 if __name__ == '__main__':
